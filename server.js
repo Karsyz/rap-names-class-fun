14368ec3cd2a78352cf79266fac8ae691d6432a3
Karsyz <mattkars@gmail.com>
2022-08-19 20:00:08 -0400

    changes

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/:rapperName', (req, res) => {
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}.`)
 })
